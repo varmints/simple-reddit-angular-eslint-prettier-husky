4d120e371a433956d4a8a96870ce33630b32a6a8
Twoje Imię Nazwisko <toja@example.com>
2021-10-17 21:01:02 +0200

    simple reedit app base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'https://angular.io', 3),
+      new Article('Fullstack', 'https://fullstack.io', 2),
+      new Article('Angular Homepage', 'https://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+    }
 }
---
+++ src/app/article/article.component.ts
@@ -0,0 +1,32 @@
+import { Component, HostBinding, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent  {
+  // By using the @HostBinding decorator, we’re able to configure our host element from within the component.
+  @HostBinding('attr.class') cssClass = '';
+  @Input() article!: Article;
+
+  constructor() {
+    // article is populated by the Input now,
+    // so we don't need anything here
+  }
+
+  // ngOnInit(): void {
+  // }
+
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+    }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+}
